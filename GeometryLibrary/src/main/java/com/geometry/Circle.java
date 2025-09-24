f97c8d58c1f3c4834a23b3133414db8ae7f5c4cc
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-09-24 10:42:25 +0300

    All changes

---
+++ GeometryLibrary/src/main/java/com/geometry/Circle.java
@@ -0,0 +1,22 @@
+package com.geometry;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
---
+++ GeometryLibrary/src/main/java/com/geometry/Triangle.java
@@ -0,0 +1,35 @@
+package com.geometry;
+
+public class Triangle {
+
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public double getArea() {
+        double s = getPerimeter() / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+}
